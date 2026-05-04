eea25413287226af08ebfe068bd0d438af9a6764
Mariano Corbalan <mc017c@gmail.com>
2026-05-04 15:49:06 -0300

    Resuelto problema3

---
+++ Dsw2026Ej8/Problema3/Problema3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema3
+{    public class Problema3
+    {    public string CompararCopias(int originalValue, Product product)
+        {       int valorCopia = originalValue;             valorCopia++;
+                Product productoCopiado = product; 
+                productoCopiado.ModificarDescripcion("Descripcion Modificada");
+
+            return $"{originalValue}-{valorCopia}-{product.Description}";
+        }
+    }
+}
---
+++ Dsw2026Ej8/Problema3/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Dsw2026Ej8.Problema3
+{    public class Product
+    {public string Description {get; set;}
+        public void ModificarDescripcion(string nuevaDescripcion)
+        {
+                    Description = nuevaDescripcion;
+        }
+    }
+}
