bb697a38000105d5ea1643165fc334024ed6b3a2
Mariano Corbalan <mc017c@gmail.com>
2026-05-04 16:14:03 -0300

    resuelto problema 5

---
+++ Dsw2026Ej8/Problema5/Problema5.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema5
+{public class Problema5
+    {public decimal ObtenerImporteFinal(Sale sale)
+        {return sale.CalculateTotal();
+        }
+    }
+}
---
+++ Dsw2026Ej8/Problema5/Sales.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema5
+{    public class Sale
+{    public decimal Subtotal {get; set;} = 1000m;
+     public virtual decimal CalculateTotal()
+            {   return Subtotal;        }
+}
+     public class RetailSale : Sale
+    {
+        public override decimal CalculateTotal()
+        {      return Subtotal;      }
+    }
+    public class WholesaleSale : Sale
+    {
+        public override decimal CalculateTotal()
+        {       return Subtotal - (Subtotal * 0.10m);
+        }
+    }
+}
