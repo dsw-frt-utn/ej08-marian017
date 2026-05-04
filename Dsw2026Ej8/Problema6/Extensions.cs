a8f4029c49e52cc9289d274242dff797d67dfef5
Mariano Corbalan <mc017c@gmail.com>
2026-05-04 16:23:10 -0300

    resuelto problema 6

---
+++ Dsw2026Ej8/Problema6/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema6
+{   public static class Extensions
+    {   public static string ToProductCode(this string code)
+        {   if (string.IsNullOrWhiteSpace(code))
+            {return "SIN-CODIGO"; //[cite: 1]
+            }
+            return code.Trim().ToUpper().Replace(" ", "-");
+        }
+    }
+}
---
+++ Dsw2026Ej8/Problema6/Problema6.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema6
+{   public class Problema6
+    {   public string NormalizarCodigoProducto(string code)
+        {      return code.ToProductCode();
+        }
+    }
+}
